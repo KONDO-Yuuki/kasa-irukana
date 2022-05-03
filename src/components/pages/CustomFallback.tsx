128822ca6c1bed2dc050335fa78f98358d5487df
yuki <yuki>
2022-05-03 17:40:47 +0900

    CustomFallBackがErrorメッセージを表示するようにした

--- src/components/pages/CustomFallback.tsx
+++ src/components/pages/CustomFallback.tsx
@@ -5,12 +5,14 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 
 type Props = {
   resetError: () => void;
+  error: Error;
 };
 
-export const CustomFallback: React.FC<Props> = ({resetError}) => (
+export const CustomFallback: React.FC<Props> = ({resetError, error}) => (
   <SafeAreaView style={styles.container}>
     <Layout style={styles.layoutContainer} level="4">
       <Text category="h1">{'エラーが発生しました'}</Text>
+      <Text>{error.message}</Text>
       <Button onPress={resetError}>{'やりなおす'}</Button>
     </Layout>
   </SafeAreaView>
